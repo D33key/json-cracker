ad50c2adc8dabd4bf944f5cb0358eda271ba4c8a
unknown <dimakisa777@yandex.ru>
2023-11-16 12:50:27 +0300

    feat: creating layot for project

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import './index.css';
+import { Layout, Space } from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+	textAlign: "center",
+	color: "#fff",
+	height: 64,
+	paddingInline: 50,
+	lineHeight: "64px",
+	backgroundColor: "#7dbcea",
+};
+
+const contentStyle: React.CSSProperties = {
+	textAlign: "center",
+	minHeight: 120,
+	lineHeight: "120px",
+	color: "#fff",
+	backgroundColor: "#108ee9",
+};
+
+const siderStyle: React.CSSProperties = {
+	textAlign: "center",
+	lineHeight: "120px",
+	color: "#fff",
+	backgroundColor: "#3ba0e9",
+};
+
+const footerStyle: React.CSSProperties = {
+	textAlign: "center",
+	color: "#fff",
+	backgroundColor: "#7dbcea",
+};
+
+function App() {
+	return (
+		<Space direction="vertical" style={{ width: "100%", height: '100vh' }} size={[0, 48]}>
+			<Layout >
+				<Sider style={siderStyle}>Sider</Sider>
+				<Layout>
+					<Header style={headerStyle}>Header</Header>
+					<Content style={contentStyle}>Content</Content>
+					<Footer style={footerStyle}>Footer</Footer>
+				</Layout>
+			</Layout>
+		</Space>
+	);
+}
+
+export default App;
