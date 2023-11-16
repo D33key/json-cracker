307d54e47ed3f2152b6c63b9257fbdc293024f37
unknown <dimakisa777@yandex.ru>
2023-11-16 13:15:48 +0300

    feat: add styles for header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import './index.css';
+import "./styles/index.css";
 import { Layout, Space } from "antd";
 
 const { Header, Footer, Sider, Content } = Layout;
-
-const headerStyle: React.CSSProperties = {
-	textAlign: "center",
-	color: "#fff",
-	height: 64,
-	paddingInline: 50,
-	lineHeight: "64px",
-	backgroundColor: "#7dbcea",
-};
 
 const contentStyle: React.CSSProperties = {
 	textAlign: "center",
@@ -21,28 +12,23 @@ const contentStyle: React.CSSProperties = {
 };
 
 const siderStyle: React.CSSProperties = {
+	minWidth: 400,
 	textAlign: "center",
 	lineHeight: "120px",
 	color: "#fff",
 	backgroundColor: "#3ba0e9",
 };
 
-const footerStyle: React.CSSProperties = {
-	textAlign: "center",
-	color: "#fff",
-	backgroundColor: "#7dbcea",
-};
-
 function App() {
 	return (
-		<Space direction="vertical" style={{ width: "100%", height: '100vh' }} size={[0, 48]}>
-			<Layout >
-				<Sider style={siderStyle}>Sider</Sider>
-				<Layout>
-					<Header style={headerStyle}>Header</Header>
+		<Space direction="vertical" style={{ width: "100%" }} size={[0, 48]}>
+			<Layout style={{ height: "100vh" }}>
+				<Header className="custom-maintains custom-header"></Header>
+				<Layout hasSider>
+					<Sider style={siderStyle}>Sider</Sider>
 					<Content style={contentStyle}>Content</Content>
-					<Footer style={footerStyle}>Footer</Footer>
 				</Layout>
+				<Footer className="custom-maintains custom-footer"></Footer>
 			</Layout>
 		</Space>
 	);
