758b1a23be04dad45fc6304fbc7116ca85126581
D33key <dimakisa777@yandex.ru>
2023-11-17 16:17:19 +0300

    fix: update remove method (not deleting element from root)

--- src/core/lib/toronto.ts
+++ src/core/lib/toronto.ts
@@ -2,50 +2,65 @@ export class Toronto {
 	private root: HTMLElement | null;
 	private map = new Map<number, CustomDivElement>();
 	constructor(root: string) {
-		this.root = document.querySelector(`${root}`);
+		this.root = document.querySelector(root);
 		this.init();
 	}
 
 	createAndInsertElement() {
+		if (!this.root) {
+			throw new Error("Root element not found.");
+		}
 		const indexForElement = this.map.size;
 		const element = new CustomDivElement(indexForElement);
 		const htmlElement = element.create();
 
 		this.map.set(indexForElement, element);
 
-		this.root?.append(htmlElement);
+		this.root.append(htmlElement);
 
 		return this;
 	}
 
 	removeElement(index: number) {
-		this.map.delete(index);
+		const elementToRemove = this.map.get(index);
+		if (elementToRemove) {
+			elementToRemove.destroy();
+			this.map.delete(index);
+		}
 	}
 
 	private init() {
+		if (!this.root) {
+			throw new Error("Root element not found.");
+		}
 		if (this.map.size === 0) {
 			this.createAndInsertElement();
 		}
-		this.root?.addEventListener("click", (event: MouseEvent) => {
+		this.root.addEventListener("click", (event: MouseEvent) => {
 			const target = event.target as HTMLDivElement;
 
-			
+			if (target && target.classList.contains("view-line")) {
+				const indexOfTarget = Number(target.dataset.indexNumber);
+				const getCustomDivElement = this.map.get(indexOfTarget);
+				getCustomDivElement?.editable();
+			}
 		});
 	}
 }
 
 class CustomDivElement {
 	index: number;
+	newElement: HTMLDivElement;
 	constructor(index: number) {
 		this.index = index;
+		this.newElement = document.createElement("div");
 	}
 
 	create() {
-		const newElement = document.createElement("div");
-		newElement.classList.add("view-line");
-		newElement.dataset.indexNumber = String(this.index);
+		this.newElement.classList.add("view-line");
+		this.newElement.dataset.indexNumber = String(this.index);
 
-		return newElement;
+		return this.newElement;
 	}
 
 	get indexOfElement() {
@@ -54,5 +69,23 @@ class CustomDivElement {
 
 	set indexOfElement(index: number) {
 		this.index = index;
+	}
+
+	editable() {
+		const blurHandler = (ev: FocusEvent) => {
+			const element = ev.currentTarget as HTMLDivElement;
+			element.removeAttribute("contenteditable");
+			element.removeEventListener("blur", blurHandler);
+		};
+
+		this.newElement.setAttribute("contenteditable", "true");
+		this.newElement.focus();
+		this.newElement.addEventListener("blur", blurHandler);
+	}
+
+	destroy() {
+		if (this.newElement.parentElement) {
+			this.newElement.parentElement.removeChild(this.newElement);
+		}
 	}
 }
